8d604f9bb192be88e92519bb616ed8243b0e4250
barakaally <baraka.ally.co.tz@gmail.com>
2021-04-11 17:59:05 +0300

    format niches

--- src/main/java/youtube/data/niche/PlayerResponse.java
+++ src/main/java/youtube/data/niche/PlayerResponse.java
@@ -1,32 +1,30 @@
 ﻿package youtube.data.niche;
 
+public class PlayerResponse {
+    private PlayabilityStatus playabilityStatus;
+    private StreamingData streamingData;
+    private videoDetail videoDetails;
+    private PlayerConfig playerConfig;
+    private Microformat microformat;
 
-public class PlayerResponse
-    {
-        private PlayabilityStatus playabilityStatus;
-        private StreamingData streamingData;
-        private videoDetail videoDetails;
-        private PlayerConfig playerConfig;
-        private Microformat microformat;
-       
-        public PlayabilityStatus getPlayabilityStatus() {
-            return playabilityStatus;
-        }
-        
-        public StreamingData getStreamingData() {
-            return streamingData;
-        }
-       
-        public videoDetail getVideoDetails() {
-            return videoDetails;
-        }
-    
-        public PlayerConfig getPlayerConfig() {
-            return playerConfig;
-        }
-
-        public Microformat getMicroformat() {
-            return microformat;
-        }
-      
+    public PlayabilityStatus getPlayabilityStatus() {
+        return playabilityStatus;
     }
+
+    public StreamingData getStreamingData() {
+        return streamingData;
+    }
+
+    public videoDetail getVideoDetails() {
+        return videoDetails;
+    }
+
+    public PlayerConfig getPlayerConfig() {
+        return playerConfig;
+    }
+
+    public Microformat getMicroformat() {
+        return microformat;
+    }
+
+}
--- src/main/java/youtube/data/niche/Response.java
+++ src/main/java/youtube/data/niche/Response.java
@@ -1,13 +1,12 @@
 ﻿package youtube.data.niche;
 
-  public class Response
-   {
-        public Object errors;
-        public VideoInfo data;
-        public Response(Object errors, VideoInfo data) {
-          this.errors = errors;
-          this.data = data;
-     }
-        
-   }
+public class Response {
+  public Object errors;
+  public VideoInfo data;
 
+  public Response(Object errors, VideoInfo data) {
+    this.errors = errors;
+    this.data = data;
+  }
+
+}
