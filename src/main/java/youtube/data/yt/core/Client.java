9d8a4c91600c088f4f51d9f5a3e7412f2fe190d2
barakaally <baraka.ally.co.tz@gmail.com>
2021-04-15 22:50:39 +0300

    .

--- src/main/java/youtube/data/yt/core/Client.java
+++ src/main/java/youtube/data/yt/core/Client.java
@@ -260,7 +260,7 @@ public abstract class Client {
     protected String searchV3(String searchText, int limit) throws Exception {
 
         if (apiKey != null) {
-            HttpsURLConnection mConnection = (HttpsURLConnection) new URL(String.format("https://www.googleapis.com/youtube/v3/search?key=%s&part=snippet,id&order=date&maxResults=%d&q=%s",apiKey, limit, searchText.replaceAll("\s", "%20"))).openConnection();
+            HttpsURLConnection mConnection = (HttpsURLConnection) new URL(String.format("https://www.googleapis.com/youtube/v3/search?key=%s&part=snippet,id&order=date&maxResults=%d&q=%s",apiKey, limit,searchText.replaceAll("\s", "%20"))).openConnection();
             mConnection.setRequestProperty("Accept-Language", Locale.getDefault().getLanguage());
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(mConnection.getInputStream()));
